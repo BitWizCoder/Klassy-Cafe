f332977dd428db279ee148c058d175465f395561
Muhammad Noman <mhmdnoman01@gmail.com>
2025-09-07 14:59:44 +0600

    finished hero

--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="flex mt-12">
-      <div class="w-1/4 bg-indigo-500 bg-[url(images/top-left-bg.jpg)] bg-cover bg-center">
-        <h2>KlassyCafe</h2>
+    <div className="flex flex-col md:flex-row mt-12">
+      <div class="w-full md:w-1/4 bg-indigo-500 bg-[url(images/top-left-bg.jpg)] bg-cover bg-center flex flex-col items-center justify-center text-white">
+        <h2 className="text-4xl mt-4 font-dancing-script italic">KlassyCafe</h2>
         <p>THE BEST EXPERIENCE</p>
-        <button>Make A Reservation</button>
+        <button className="bg-white py-2 px-4 rounded-sm text-primary-500 cursor-pointer my-4">
+          Make A Reservation
+        </button>
       </div>
-      <div class="w-3/4 bg-amber-600">
+      <div class="w-full md:w-3/4 bg-amber-600">
         <img
           src="images/slide-02.jpg"
           alt="resturant"
