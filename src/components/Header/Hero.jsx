ec1ea9e3ee8ffdc51922e4573bbbc11f8e11bf5b
Muhammad Noman <mhmdnoman01@gmail.com>
2025-09-07 12:55:23 +0600

    added hero images

--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div>
-      
+    <div className="flex mt-12">
+      <div class="w-1/4 bg-indigo-500 bg-[url(images/top-left-bg.jpg)] bg-cover bg-center">
+        <h2>KlassyCafe</h2>
+        <p>THE BEST EXPERIENCE</p>
+        <button>Make A Reservation</button>
+      </div>
+      <div class="w-3/4 bg-amber-600">
+        <img
+          src="images/slide-02.jpg"
+          alt="resturant"
+          className="bg-cover bg-center"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
