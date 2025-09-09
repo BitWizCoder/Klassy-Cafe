2f62acd8ea4cbc3c4673fea7720c7da768e195ac
Muhammad Noman <mhmdnoman01@gmail.com>
2025-09-09 17:46:57 +0600

    updated cta btn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <div class="w-full md:w-1/4 bg-indigo-500 bg-[url(images/top-left-bg.jpg)] bg-cover bg-center flex flex-col items-center justify-center text-white">
         <h2 className="text-4xl mt-4 font-dancing-script italic">KlassyCafe</h2>
         <p>THE BEST EXPERIENCE</p>
-        <button className="bg-white py-2 px-4 rounded-sm text-primary-500 cursor-pointer my-4">
+        <button className="bg-white hover:bg-gray-100 py-2 px-4 rounded-sm text-primary-500 cursor-pointer my-4">
           Make A Reservation
         </button>
       </div>
